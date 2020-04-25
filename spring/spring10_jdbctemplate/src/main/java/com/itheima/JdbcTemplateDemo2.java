4b32910ac7f729d4e811f8e8460892d95bba17e5
dustyou <1172073068@qq.com>
2020-04-25 21:10:50 +0800

    spring10_jdbc完善

--- spring/spring10_jdbctemplate/src/main/java/com/itheima/JdbcTemplateDemo2.java
+++ spring/spring10_jdbctemplate/src/main/java/com/itheima/JdbcTemplateDemo2.java
@@ -1,5 +1,7 @@
 package com.itheima;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -19,7 +21,13 @@ public class JdbcTemplateDemo2 {
 //        //2. 执行操作
 //        jt.execute("insert into account(name,money) values('ccc',1000)");
     
-    
+    //1. 获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2. 获取对象
+        JdbcTemplate jt = ac.getBean("jdbcTemplate", JdbcTemplate.class);
+        //3. 执行操作
+        jt.execute("insert into account(name,money) values('ddd',2222)");
+        
     
     
     
